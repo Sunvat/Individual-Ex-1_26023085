892532772ae6145756d1cb26e4ce7233c29dcc33
Sunvat <sunvat14@gmail.com>
2022-09-16 12:32:41 -0700

    Created new main file for methods. Made minor changes to sample file.

--- sample.java
+++ sample.java
@@ -1,5 +1,17 @@
 import java.util.*;
 public class sample {
-	public static void main(String[]args) {
+    public static void sumOfDigits() {
+        Scanner in = new Scanner(System.in);
+        int a, s = 0, g;
+        System.out.println("ENTER AN INTEGER BETWEEN 0 AND 1000");
+        a = in.nextInt();
+        while (a != 0) {
+            g = a % 10;
+            s = s + g;
+            a = a / 10;
+        }
+        System.out.println("SUM OF DIGITS : " + s);
+
     }
 }
+
